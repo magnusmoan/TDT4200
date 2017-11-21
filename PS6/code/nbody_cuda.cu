5900e979e63bd296ee17388cb942c9109b46beb8
Magnus Moan <magnus.moan@gmail.com>
2017-11-21 22:09:37 +0100

    Task 4,5,6,7

--- PS6/code/nbody_cuda.cu
+++ PS6/code/nbody_cuda.cu
@@ -99,16 +99,41 @@ __device__ float2 calculate_velocity_change_planet(float4 p, float4 q){
 
 // TODO 5. Calculate the change in velocity for my_planet, caused by the interactions with a block of planets
 __device__ float2 calculate_velocity_change_block(float4 my_planet, float4* shared_planets){
+    float2 v = float2(0.0, 0.0);
+    int i;
 
+    for(i = 0; i < blockDim.x; ++i) {
+        float2 change = calculate_velocity_change_planet(my_planet, shared_planets[i]);
+	v.x += change.x;
+	v.y += change.y;
+    }
+
+    return v;
 }
 
 // TODO 4. Update the velocities by calculating the planet interactions
 __global__ void update_velocities(float4* planets, float2* velocities, int num_planets){
+    int id = threadIdx.x + blockIdx.x * blockDim.x;
+    float4 planet = planets[id];
 
+    __shared__ float4 shared_planets[BLOCK_SIZE];
+    int i;
+    for(i = 0; i < num_planets; i+=blockDim.x) {
+	shared_planets[threadIdx.x] = planets[i + threadIdx.x];
+	__syncthreads();
+
+	float2 v = calculate_velocity_change_block(my_planet, shared_planets);
+	velocities[id].x += v.x;
+	velocities[id].y += v.y;
+	__syncthreads();
+    }
 }
 
 // TODO 7. Update the positions of the planets using the new velocities
 __global__ void update_positions(float4* planets, float2* velocities, int num_planets){
+    int id = threadIdx.x + blockIdx.x * blockDim.x;
+    planets[id].x += velocities[id].x * dT;
+    planets[id].y += velocities[id].y * dT;
 
 }
 
